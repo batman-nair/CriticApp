3e561bc814b1d8695b38d7fc2a114973516eddf1
Arjun Nair <arjunnair.mec@gmail.com>
2022-01-18 11:36:08 +0530

    Init commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath critic critic.wsgi --log-file -
---
+++ critic/addmovie/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AddmovieConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'addmovie'
