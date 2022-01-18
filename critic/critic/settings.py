c85253453ff1b68e3883dd21cee046add360df15
Arjun Nair <arjunnair.mec@gmail.com>
2022-01-18 11:52:03 +0530

    Add static stuff

--- critic/critic/settings.py
+++ critic/critic/settings.py
@@ -127,11 +127,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 
 STATIC_URL = '/static/'
-
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
