0a2ea43e0f1975e2c3f044e68b1a1d3564f26b76
Arjun Nair <arjunnair.mec@gmail.com>
2022-01-22 11:54:11 +0530

    Add auth check for API

--- critic/movie/views.py
+++ critic/movie/views.py
@@ -6,23 +6,34 @@ from dotenv import load_dotenv, find_dotenv
 import requests
 import json
 
-# Create your views here.
-
 load_dotenv(find_dotenv())
 OMDB_API = os.environ['OMDB_API']
 BASE_URL = 'http://www.omdbapi.com/?apikey={}&'.format(OMDB_API)
+INVALID_USER_RESPONSE = {"Response": "False", "Error": "User not authenticated."}
+NOT_OK_RESPONSE = {"Response": "False", "Error": "Bad reponse from API."}
 
 def get_movie_info(request, imdb_id):
+    if not request.user.is_authenticated:
+        return JsonResponse(INVALID_USER_RESPONSE)
     info_url = '{base_url}i={imdb_id}'.format(base_url=BASE_URL, imdb_id=imdb_id)
     r = requests.get(info_url)
-    print(r.json(), '||||||', str(r), '|||||', info_url, 'base_url')
-    return JsonResponse(r.json())
+    if r.status_code == 200:
+        return JsonResponse(r.json())
+    else:
+        response = NOT_OK_RESPONSE.copy()
+        response["status_code"] = r.status_code
+        response["imdb_id"] = imdb_id
+        return JsonResponse(response)
 
 def search_movie(request, movie_name):
+    if not request.user.is_authenticated:
+        return JsonResponse(INVALID_USER_RESPONSE)
     search_url = '{base_url}s={movie_name}'.format(base_url=BASE_URL, movie_name=movie_name)
     r = requests.get(search_url)
     if r.status_code == 200:
-        print(r.json(), '||||||', str(r), '|||||', movie_name, 'base_url')
         return JsonResponse(r.json())
     else:
-        return HttpResponse('Error {} when fetching data in {} for movie {}'.format(r.status_code, search_url, movie_name))
+        response = NOT_OK_RESPONSE.copy()
+        response["status_code"] = r.status_code
+        response["movie_name"] = movie_name
+        return JsonResponse(response)
