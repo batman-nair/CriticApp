62c556e4869c242c926e4b3e0727590e56ff9049
Arjun Nair <arjunnair.mec@gmail.com>
2022-01-25 12:31:23 +0530

    Add Movie model and saving to db

--- critic/movie/views.py
+++ critic/movie/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+from .models import Movie
+
 import os
 from dotenv import load_dotenv, find_dotenv
 import requests
@@ -15,10 +17,21 @@ NOT_OK_RESPONSE = {"Response": "False", "Error": "Bad reponse from API."}
 def get_movie_info(request, imdb_id):
     if not request.user.is_authenticated:
         return JsonResponse(INVALID_USER_RESPONSE)
+    try:
+        movie = Movie.objects.get(imdb_id=imdb_id)
+        print('Found movie {} in db.'.format(movie.title))
+        return JsonResponse(movie.to_omdb_json())
+    except Movie.DoesNotExist:
+        print('Movie does not exist in db, fetching from API')
     info_url = '{base_url}i={imdb_id}'.format(base_url=BASE_URL, imdb_id=imdb_id)
     r = requests.get(info_url)
     if r.status_code == 200:
-        return JsonResponse(r.json())
+        json_data = r.json()
+        if json_data['Response'] == 'True':
+            movie = Movie.from_omdb_json(**json_data)
+            movie.save()
+            print('Saving movie {} to db'.format(movie.title))
+        return JsonResponse(json_data)
     else:
         response = NOT_OK_RESPONSE.copy()
         response["status_code"] = r.status_code
