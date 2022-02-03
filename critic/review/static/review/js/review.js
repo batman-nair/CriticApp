90a09f1e7de75e310e48e9b2328b433baa2b51a8
Arjun Nair <arjunnair.mec@gmail.com>
2022-02-03 12:22:05 +0530

    Add category to reviews

--- critic/review/static/review/js/review.js
+++ critic/review/static/review/js/review.js
@@ -1,7 +1,7 @@
 const baseUrl = window.location.origin
 
-async function getSearchItems(query) {
-    const searchUrl = baseUrl + '/search_item/movie/' + query
+async function getSearchItems(category, query) {
+    const searchUrl = `${baseUrl}/search_item/${category}/${query}`
     const response = await fetch(searchUrl)
     const data = await response.json()
     console.log('Got data from search', query, data)
@@ -12,21 +12,24 @@ async function getSearchItems(query) {
     return data["Search"]
 }
 
-async function getReviewItem(itemID) {
-    const response = await fetch(baseUrl + '/get_item_info/movie/' + itemID)
+async function getReviewItem(category, itemID) {
+    const response = await fetch(`${baseUrl}/get_item_info/${category}/${itemID}`)
     const data = await response.json()
     console.log('Got review details: ', data)
     return data
 }
 
-async function updateReviewItem(itemID, reviewCard) {
-    const data = await getReviewItem(itemID)
+async function updateReviewItem(category, itemID, reviewCard) {
+    const data = await getReviewItem(category, itemID)
     console.log('Got review item data: ', data)
     reviewCard.querySelector(".review-title").innerHTML = data["Title"] + " " + reviewCard.querySelector(".year").outerHTML
-    reviewCard.querySelector("img").src = data["ImageURL"]
+    if (data["ImageURL"] != "N/A") {
+        reviewCard.querySelector("img").src = data["ImageURL"]
+    }
     reviewCard.querySelector(".year").innerText = data["Year"]
     reviewCard.querySelector(".attr1").innerText = data["Attr1"]
-    reviewCard.querySelector(".description").innerText = data["Description"]
+    reviewCard.querySelector(".description").innerHTML = data["Description"]
     reviewCard.querySelector(".attr2").innerText = data["Attr2"]
+    reviewCard.querySelector(".rating").innerText = data["Rating"]
     reviewCard.removeAttribute("hidden")
 }
