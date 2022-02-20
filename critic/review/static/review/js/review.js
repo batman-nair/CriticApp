ba343e76b2f1da203bda3bce599c62154ecd22b4
Arjun Nair <arjunnair.mec@gmail.com>
2022-02-20 22:34:56 +0530

    Add review cards to be viewed with masonry

--- critic/review/static/review/js/review.js
+++ critic/review/static/review/js/review.js
@@ -47,11 +47,55 @@ async function getReviews(query='', username='', filter_categories=[]) {
     if (username) {
         reviewUrl.searchParams.append('username', username)
     }
-    for (category in filter_categories) {
+    for (category of filter_categories) {
         reviewUrl.searchParams.append('filter_categories', category)
     }
     const response = await fetch(reviewUrl)
     const data = await response.json()
     console.log('Got reviews', data)
-    return data["Results"]
+    return [...data["Results"], ...data["Results"]]
+}
+
+function buildReviewCard(review) {
+    console.log('Review:', review)
+    const reviewCardWrapper = document.createElement("div")
+    reviewCardWrapper.classList.add('mb-4')
+    if (review.ReviewItem.Category == 'game') {
+        reviewCardWrapper.classList.add('col-6', 'col-lg-6')
+    }
+    else {
+        reviewCardWrapper.classList.add('col-6', 'col-lg-3')
+    }
+    const content = `
+        <div class="card review-card">
+            <img src="${review.ReviewItem.ImageURL}" class="card-img-top" alt="Poster Image">
+            <div class="card-body">
+                <h5 class="card-title">${review.ReviewItem.Title}</h5>
+                <p class="card-text"><small class="attr1 text-muted">${review.ReviewItem.Attr1}</small></p>
+                <p class="description card-text">${review.ReviewData}</p>
+                <p class="rating card-text">${review.Rating}⭐</p>
+            </div>
+        </div>
+    `
+    reviewCardWrapper.innerHTML = content
+    return reviewCardWrapper
+}
+
+function populateReviewCards(parentSelector, reviews) {
+    const container = document.querySelector(parentSelector)
+    container.innerHTML = ""
+    console.log('Reviews:', reviews)
+    for (var review of reviews) {
+        const reviewCard = buildReviewCard(review)
+        container.appendChild(reviewCard)
+    }
+}
+
+// From freecodecamp
+function debounce(func, timeout=300) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    }
 }
