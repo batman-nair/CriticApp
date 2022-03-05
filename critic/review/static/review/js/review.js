6b8653867cd375c74ebdca417a0b6f138159f75e
Arjun Nair <arjunnair.mec@gmail.com>
2022-03-05 12:51:29 +0530

    JS styling, add semi colons

--- critic/review/static/review/js/review.js
+++ critic/review/static/review/js/review.js
@@ -1,27 +1,27 @@
-const baseUrl = window.location.origin
+const baseUrl = window.location.origin;
 
 async function getSearchItems(category, query) {
-    const searchUrl = `${baseUrl}/search_item/${category}/${query}`
-    const response = await fetch(searchUrl)
-    const data = await response.json()
-    console.log('Got data from search', query, data)
+    const searchUrl = `${baseUrl}/search_item/${category}/${query}`;
+    const response = await fetch(searchUrl);
+    const data = await response.json();
+    console.log('Got data from search', query, data);
     if (data["Response"] == "False") {
-        console.log("Got error from search ", query, data["Error"])
-        return []
+        console.log("Got error from search ", query, data["Error"]);
+        return [];
     }
-    return data["Results"]
+    return data["Results"];
 }
 
 async function getReviewItem(category, itemID) {
-    const response = await fetch(`${baseUrl}/get_item_info/${category}/${itemID}`)
-    const data = await response.json()
-    console.log('Got review details: ', data)
-    return data
+    const response = await fetch(`${baseUrl}/get_item_info/${category}/${itemID}`);
+    const data = await response.json();
+    console.log('Got review details: ', data);
+    return data;
 }
 
 async function updateReviewItem(category, itemID, reviewCard) {
-    const data = await getReviewItem(category, itemID)
-    console.log('Got review item data: ', data)
+    const data = await getReviewItem(category, itemID);
+    console.log('Got review item data: ', data);
     let titleDOM = reviewCard.querySelector(".review-title");
     let yearDOM = reviewCard.querySelector(".review-year");
     let imageDOM = reviewCard.querySelector("img");
@@ -31,60 +31,60 @@ async function updateReviewItem(category, itemID, reviewCard) {
     } else {
         imageDOM.src = "";
     }
-    reviewCard.querySelector(".review-year").innerText = data["Year"]
-    reviewCard.querySelector(".review-attr1").innerText = data["Attr1"]
-    console.log(reviewCard.querySelector("img").height, reviewCard.querySelector("img").width)
+    reviewCard.querySelector(".review-year").innerText = data["Year"];
+    reviewCard.querySelector(".review-attr1").innerText = data["Attr1"];
+    console.log(reviewCard.querySelector("img").height, reviewCard.querySelector("img").width);
     let descriptionDOM = reviewCard.querySelector(".review-description");
     let descriptionDOMBelow = reviewCard.querySelector(".review-description-below");
-    console.log('Details', imageDOM.width, imageDOM.height, data["Description"].length)
+    console.log('Details', imageDOM.width, imageDOM.height, data["Description"].length);
     if (imageDOM.width > imageDOM.height || data["Description"].length > 300) {
         descriptionDOM.innerHTML = "";
         descriptionDOM = descriptionDOMBelow;
         descriptionDOMBelow.removeAttribute("hidden");
     } else {
-        descriptionDOMBelow.innerHTML = ""
+        descriptionDOMBelow.innerHTML = "";
         descriptionDOMBelow.setAttribute("hidden", "");
     }
-    descriptionDOM.innerHTML = data["Description"]
-    reviewCard.querySelector(".review-attr2").innerText = data["Attr2"]
-    reviewCard.querySelector(".review-rating").innerText = data["Rating"]
-    reviewCard.removeAttribute("hidden")
+    descriptionDOM.innerHTML = data["Description"];
+    reviewCard.querySelector(".review-attr2").innerText = data["Attr2"];
+    reviewCard.querySelector(".review-rating").innerText = data["Rating"];
+    reviewCard.removeAttribute("hidden");
 }
 
 function updateForm(category, itemID) {
-    document.querySelector("#id_item_id").value = itemID
-    document.querySelector("#id_category").value = category
-    document.querySelector("#review-form").removeAttribute("disabled")
+    document.querySelector("#id_item_id").value = itemID;
+    document.querySelector("#id_category").value = category;
+    document.querySelector("#review-form").removeAttribute("disabled");
 }
 
 async function getReviews(query='', username='', filter_categories=[], ordering='') {
-    const reviewUrl = new URL(`${baseUrl}/reviews`)
+    const reviewUrl = new URL(`${baseUrl}/reviews`);
     if (query) {
-        reviewUrl.searchParams.append('query', query)
+        reviewUrl.searchParams.append('query', query);
     }
     if (username) {
-        reviewUrl.searchParams.append('username', username)
+        reviewUrl.searchParams.append('username', username);
     }
     for (category of filter_categories) {
-        reviewUrl.searchParams.append('filter_categories', category)
+        reviewUrl.searchParams.append('filter_categories', category);
     }
     if (ordering) {
-        reviewUrl.searchParams.append('ordering', ordering)
+        reviewUrl.searchParams.append('ordering', ordering);
     }
-    const response = await fetch(reviewUrl)
-    const data = await response.json()
-    console.log('Got reviews', data)
-    return [...data["Results"], ...data["Results"]]
+    const response = await fetch(reviewUrl);
+    const data = await response.json();
+    console.log('Got reviews', data);
+    return [...data["Results"], ...data["Results"]];
 }
 
 function buildReviewCard(review) {
-    const reviewCardWrapper = document.createElement("div")
-    reviewCardWrapper.classList.add('mb-4')
+    const reviewCardWrapper = document.createElement("div");
+    reviewCardWrapper.classList.add('mb-4');
     if (review.ReviewItem.Category == 'game') {
-        reviewCardWrapper.classList.add('col-6', 'col-lg-6')
+        reviewCardWrapper.classList.add('col-6', 'col-lg-6');
     }
     else {
-        reviewCardWrapper.classList.add('col-6', 'col-lg-3')
+        reviewCardWrapper.classList.add('col-6', 'col-lg-3');
     }
     const content = `
         <div class="card review-card">
@@ -96,17 +96,17 @@ function buildReviewCard(review) {
                 <p class="rating card-text">${review.Rating}⭐</p>
             </div>
         </div>
-    `
-    reviewCardWrapper.innerHTML = content
-    return reviewCardWrapper
+    `;
+    reviewCardWrapper.innerHTML = content;
+    return reviewCardWrapper;
 }
 
 function populateReviewCards(parentSelector, reviews) {
-    const container = document.querySelector(parentSelector)
-    container.innerHTML = ""
+    const container = document.querySelector(parentSelector);
+    container.innerHTML = "";
     for (var review of reviews) {
-        const reviewCard = buildReviewCard(review)
-        container.appendChild(reviewCard)
+        const reviewCard = buildReviewCard(review);
+        container.appendChild(reviewCard);
     }
 }
 
