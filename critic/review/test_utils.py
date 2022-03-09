426743ae130fd7a60eb03ff3c87702dacee3afd6
Arjun Nair <arjunnair.mec@gmail.com>
2022-03-09 12:21:45 +0530

    Remove old API utils functions

--- critic/review/test_utils.py
+++ critic/review/test_utils.py
@@ -4,8 +4,7 @@ from django.contrib.auth import get_user_model
 from model_bakery import baker
 
 from .models import Review, ReviewItem
-
-from .utils import review_utils as utils
+from .utils import review_utils
 from .utils import api_utils
 
 _JUNK_DATA = 'alskdjflaskjdflkasjdflkasjdflkasglhasldgfkj'
@@ -51,80 +50,6 @@ class APITest(TestCase):
         self.assertTrue(json_data["Response"], "False")
 
 
-class APITestOld(TestCase):
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(username='testuser', password='123test123')
-        self.client.login(username='testuser', password='123test123')
-
-    def tearDown(self):
-        self.user.delete()
-
-    def test_omdb_api_search(self):
-        search_json = utils.get_omdb_search('Breaking Bad')
-        self.assertEqual(search_json["Response"], "True")
-        self.assertTrue(len(search_json["Search"]) > 0)
-        search_result = search_json["Search"][0]
-        self.assertTrue(all(param in search_result for param in ["Title", "Poster", "imdbID", "Year"]))
-
-        search_json = utils.get_omdb_search('alskdjflaskjdflaksjdflkasjdlkfsajdlfksdjalfk')
-        self.assertEqual(search_json["Response"], "False")
-
-    def test_rawg_api_search(self):
-        search_json = utils.get_rawg_search('Ori and the Blind Forest')
-        self.assertEqual(search_json["Response"], "True")
-        self.assertTrue(len(search_json["results"]) > 0)
-        result = search_json["results"][0]
-        self.assertTrue(all(param in result for param in ["name", "id", "background_image", "released"]))
-
-        search_json = utils.get_rawg_search('alskdjflaskjdflaksjdflkasjdlkfsajdlfksdjalfk')
-        self.assertEqual(search_json["Response"], "False")
-
-    def check_review_search_json(self, search_json):
-        self.assertTrue("Results" in search_json)
-        self.assertEqual(search_json["Response"], "True")
-        review_search_result = search_json["Results"][0]
-        review_search_params = ["Title", "ItemID", "ImageURL", "Year"]
-        self.assertTrue(all(param in review_search_result for param in review_search_params))
-
-    def test_converted_searches(self):
-        search_json = utils.get_omdb_search('Breaking Bad')
-        review_json = utils.convert_omdb_to_review(search_json)
-        self.check_review_search_json(review_json)
-
-        invalid_search_json = utils.get_omdb_search('asdfaslkdfjlaksdjfalskdjfl')
-        review_json = utils.convert_omdb_to_review(invalid_search_json)
-        self.assertEqual(review_json["Response"], "False")
-
-        search_json = utils.get_rawg_search('Ori and the Blind Forest')
-        review_json = utils.convert_rawg_to_review(search_json)
-        self.check_review_search_json(review_json)
-
-        invalid_search_json = utils.get_rawg_search('asdfaslkdfjlaksdjfalskdjfl')
-        review_json = utils.convert_rawg_to_review(invalid_search_json)
-        self.assertEqual(review_json["Response"], "False")
-
-    def check_review_info_json(self, info_json):
-        self.assertTrue(info_json["Response"], "True")
-        review_info_params = ["Title", "ItemID", "ImageURL", "Year", "Attr1", "Attr2", "Attr3", "Description", "Rating"]
-        self.assertTrue(all(param in info_json for param in review_info_params))
-
-    def test_converted_infos(self):
-        info_json = utils.get_omdb_info('tt3896198')
-        review_json = utils.convert_omdb_to_review(info_json)
-        self.check_review_info_json(review_json)
-
-        invalid_info_json = utils.get_omdb_info('laksjdflkasjdflkasjdalskdjfl')
-        review_json = utils.convert_omdb_to_review(invalid_info_json)
-        self.assertEqual(review_json["Response"], "False")
-
-        info_json = utils.get_rawg_info('19590')
-        review_json = utils.convert_rawg_to_review(info_json)
-        self.check_review_info_json(review_json)
-
-        invalid_info_json = utils.get_rawg_info('laksjdflkasjdflkasjdalskdjfl')
-        review_json = utils.convert_rawg_to_review(invalid_info_json)
-        self.assertEqual(review_json["Response"], "False")
-
 SAMPLE_REVIEW_ITEM_JSON = {
     "Category": "movie",
     "ItemID": "test1",
@@ -147,41 +72,40 @@ class ReviewQueryTest(TestCase):
         self.review1.save()
         self.fake_reviews = [baker.make(Review) for _ in range(10)]
 
-
     def tearDown(self):
         self.user.delete()
 
     def test_basic_review_query(self):
-        reviews = utils.get_filtered_review_objects()
+        reviews = review_utils.get_filtered_review_objects()
         self.assertGreaterEqual(len(reviews), 10)
-        json_data = utils.convert_reviews_to_json(reviews)
+        json_data = review_utils.convert_reviews_to_json(reviews)
         self.assertTrue("Results" in json_data)
         self.assertGreaterEqual(len(json_data["Results"]), 10)
         review_json = json_data["Results"][0]
         self.assertTrue(all(field in review_json for field in ["ReviewData", "Rating", "ModifiedDate", "ReviewItem"]))
 
     def test_query_filter(self):
-        reviews = utils.get_filtered_review_objects(query='Cool')
+        reviews = review_utils.get_filtered_review_objects(query='Cool')
         self.assertTrue(len(reviews))
-        reviews = utils.get_filtered_review_objects(query='This should not hopefull match any random string')
+        reviews = review_utils.get_filtered_review_objects(query='This should not hopefull match any random string')
         self.assertFalse(len(reviews))
 
     def test_username_filter(self):
-        reviews = utils.get_filtered_review_objects(username='testuser')
+        reviews = review_utils.get_filtered_review_objects(username='testuser')
         self.assertTrue(len(reviews))
-        reviews = utils.get_filtered_review_objects(username='Please dont match a random string')
+        reviews = review_utils.get_filtered_review_objects(username='Please dont match a random string')
         self.assertFalse(len(reviews))
 
     def test_category_filter(self):
-        reviews = utils.get_filtered_review_objects(filter_categories=['movie'])
+        reviews = review_utils.get_filtered_review_objects(filter_categories=['movie'])
         self.assertTrue(all([review.review_item.category != 'movie' for review in reviews]))
 
     def test_ordering_filter(self):
-        reviews = utils.get_filtered_review_objects(ordering='alpha')
+        reviews = review_utils.get_filtered_review_objects(ordering='alpha')
         reviews = list(reviews)
         self.assertTrue(all(val[0].review_item.title <= val[1].review_item.title for val in zip(reviews[:-1], reviews[1:])))
 
     def test_multi_filter(self):
-        reviews = utils.get_filtered_review_objects(query='Cool', username='testuser', filter_categories=['random'])
+        reviews = review_utils.get_filtered_review_objects(query='Cool', username='testuser', filter_categories=['random'])
         self.assertEqual(len(reviews), 1)
         self.assertEqual(reviews[0].review_rating, 9.5)
--- critic/review/views.py
+++ critic/review/views.py
@@ -4,26 +4,12 @@ from django.contrib.auth.decorators import login_required
 
 from .forms import ReviewForm
 
-from .utils import review_utils as utils
-from .utils import api_utils
+from .utils import api_utils, review_utils
 from .models import ReviewItem, Review
 
-CATEGORY_TO_API = {
+CATEGORY_TO_API: dict[str, api_utils.ReviewItemAPIBase] = {
     'movie': api_utils.OMDBItemAPI(),
     'game': api_utils.RAWGItemAPI(),
-}
-
-CATEGORIES = {
-    'movie': {
-        'search': utils.get_omdb_search,
-        'info': utils.get_omdb_info,
-        'convert': utils.convert_omdb_to_review,
-    },
-    'game': {
-        'search': utils.get_rawg_search,
-        'info': utils.get_rawg_info,
-        'convert': utils.convert_rawg_to_review,
-    },
 }
 
 INVALID_USER_RESPONSE = {"Response": "False", "Error": "User not authenticated."}
@@ -31,7 +17,7 @@ INVALID_CATEGORY_RESPONSE = {"Response": "False", "Error": "Invalid category."}
 NOT_OK_RESPONSE = {"Response": "False", "Error": "Bad reponse from API."}
 
 def view_reviews(request):
-    return render(request, 'review/view_reviews.html', {'categories': CATEGORIES.keys()})
+    return render(request, 'review/view_reviews.html', {'categories': CATEGORY_TO_API.keys()})
 
 @login_required()
 def add_review(request):
@@ -65,19 +51,6 @@ def search_review_item(request, category, search_term):
     api_obj = CATEGORY_TO_API[category]
     return JsonResponse(api_obj.search(search_term))
 
-def search_review_item_old(request, category, search_term):
-    if not request.user.is_authenticated:
-        return JsonResponse(INVALID_USER_RESPONSE)
-    if category not in CATEGORIES:
-        return JsonResponse(INVALID_CATEGORY_RESPONSE)
-    util_funcs = CATEGORIES[category]
-    json_data = util_funcs['search'](search_term)
-    if json_data["Response"] == "False":
-        print('Error fetching search data from API ', json_data)
-        return JsonResponse(json_data)
-    item_data = util_funcs['convert'](json_data)
-    return JsonResponse(item_data)
-
 def get_review_item_info(request, category, item_id):
     if not request.user.is_authenticated:
         return JsonResponse(INVALID_USER_RESPONSE)
@@ -98,44 +71,18 @@ def get_review_item_info(request, category, item_id):
         review_item.save()
     return JsonResponse(item_data)
 
-def get_review_item_info_old(request, category, item_id):
-    if not request.user.is_authenticated:
-        return JsonResponse(INVALID_USER_RESPONSE)
-    if category not in CATEGORIES:
-        return JsonResponse(INVALID_CATEGORY_RESPONSE)
-    try:
-        review_item = ReviewItem.objects.get(item_id=item_id)
-        print('Got item from db {} {}'.format(category, item_id))
-        review_item_json = review_item.to_review_json()
-        review_item_json["Response"] = "True"
-        return JsonResponse(review_item_json)
-    except ReviewItem.DoesNotExist:
-        print('Didn\'t find review item {} {} in db, fetching from API'.format(category, item_id))
-    util_funcs = CATEGORIES[category]
-    json_data = util_funcs['info'](item_id)
-    if json_data["Response"] == "False":
-        print('Error fetching info data from API ', json_data)
-        return JsonResponse(json_data)
-    item_data = util_funcs['convert'](json_data)
-    item_data["Category"] = category
-    if item_data["Response"] == "True":
-        review_item = ReviewItem.from_review_json(**item_data)
-        review_item.save()
-        print('Saved item to db', category, item_id)
-    return JsonResponse(item_data)
-
 @login_required
 def profile_redirect(request):
     return redirect('review:profile', username=request.user.username)
 
 def view_profile(request, username):
-    return render(request, 'review/view_reviews.html', {'categories': CATEGORIES.keys(), 'username': username})
+    return render(request, 'review/view_reviews.html', {'categories': CATEGORY_TO_API.keys(), 'username': username})
 
 def get_reviews(request):
     query = request.GET.get('query', '')
     username = request.GET.get('username', '')
     filter_categories = request.GET.getlist('filter_categories')
     ordering = request.GET.get('ordering', '')
-    reviews = utils.get_filtered_review_objects(query, username, filter_categories, ordering)
-    json_data = utils.convert_reviews_to_json(reviews)
+    reviews = review_utils.get_filtered_review_objects(query, username, filter_categories, ordering)
+    json_data = review_utils.convert_reviews_to_json(reviews)
     return JsonResponse(json_data)
