ac0240e38f7997c73e579788ec75bb8c0cccb984
Arjun Nair <arjunnair.mec@gmail.com>
2022-03-23 11:56:41 +0530

    Add a loader for reviews

--- critic/review/static/review/js/review.js
+++ critic/review/static/review/js/review.js
@@ -51,12 +51,6 @@ async function updateReviewItem(category, itemID, reviewCard) {
     reviewCard.removeAttribute("hidden");
 }
 
-function updateForm(category, itemID) {
-    document.querySelector("#id_review_item").value = itemID;
-    document.querySelector("#id_category").value = category;
-    document.querySelector("#review-form").removeAttribute("disabled");
-}
-
 async function getReviews(query='', username='', filter_categories=[], ordering='') {
     const reviewUrl = new URL(`${baseUrl}/api/reviews`);
     if (query) {
