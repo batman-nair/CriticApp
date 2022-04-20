33a404e316131a550eb75e5c83137065c1afdf22
Arjun Nair <arjunnair.mec@gmail.com>
2022-04-20 13:01:15 +0200

    Add sample favicon and manifest

--- critic/critic/urls.py
+++ critic/critic/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', include('users.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
     path('', include('review.urls')),
 ]
