07c16c7e4ecbad0bc9f1b3fc0ec2d96f46b61f37
Arjun Nair <arjunnair.mec@gmail.com>
2024-11-14 13:18:26 +0100

    Set staticfiles_dirs and static_root

--- critic/settings/common.py
+++ critic/settings/common.py
@@ -107,6 +107,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(os.path.join(BASE_DIR, 'web', 'static')),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
